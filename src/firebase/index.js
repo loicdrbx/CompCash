f8057c88aef9866158d584caea192922a1e67582
Loic Darboux <loic.drbx@gmail.com>
2021-01-03 21:16:58 -0500

    integrate Firebase project into app.

---
+++ src/firebase/index.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDN-CKydWRnru8cCYxnkegmnLiJ7JkvlQk",
+  authDomain: "project-compost.firebaseapp.com",
+  projectId: "project-compost",
+  storageBucket: "project-compost.appspot.com",
+  messagingSenderId: "1059238678731",
+  appId: "1:1059238678731:web:bf62427ac4718c29440b4e"
+};
+
+firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
