e5877a1990acf158665d00c1829d481c93ea10ab
Harsh Patel <contact@harshpatel.ca>
2021-01-04 14:50:04 -0500

    basic styling setup

--- src/App.js
+++ src/App.js
@@ -1,47 +1,47 @@
 import './App.css';
 import logo from './logo.svg';
 
+import { Switch, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Navbar from './components/Navbar';
+
 import { db } from './firebase/index';
 import { auth } from './firebase/index';
-import AuthUI from './components/AuthUI';
 
 function App() {
-
   // Just a test to see if auth workks
   auth.onAuthStateChanged((user) => {
     if (user) {
-      console.log(user.displayName + " just logged in")
+      console.log(user.displayName + ' just logged in');
     } else {
-      console.log("No one is logged in");
+      console.log('No one is logged in');
     }
   });
 
   // Just a test to see if db works
-  db.collection("Teams").doc("Project-Compost")
+  db.collection('Teams')
+    .doc('Project-Compost')
     .set({
-      member1: "Loic",
-      member2: "Mith",
-      member3: "Qi",
-      member4: "V",
-      member5: "Harsh",
+      member1: 'Loic',
+      member2: 'Mith',
+      member3: 'Qi',
+      member4: 'V',
+      member5: 'Harsh',
     })
     .then(() => {
-      console.log("Document successfully written!");
+      console.log('Document successfully written!');
     })
     .catch((error) => {
-      console.error("Error writing document: ", error);
-    })
+      console.error('Error writing document: ', error);
+    });
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <AuthUI />
-        <p>
-          Check the <code>console</code> to monitor auth and db status.
-        </p>
-      </header>
-    </div >
+    <>
+      {/* <Navbar /> */}
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </>
   );
 }
 
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AuthUI from '../../components/AuthUI';
+
+function Home() {
+  return (
+    <div>
+      <section className="home-landing"></section>
+    </div>
+  );
+}
+
+export default Home;
