91079e4561aa4f423456c0d3f12588475b052414
Loic Darboux <loic.drbx@gmail.com>
2021-01-04 01:34:35 -0500

    add db test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import logo from './logo.svg';
 
+import { db } from './firebase/index';
 import { auth } from './firebase/index';
 import AuthUI from './components/AuthUI';
-
 
 function App() {
 
@@ -16,13 +16,29 @@ function App() {
     }
   });
 
+  // Just a test to see if db works
+  db.collection("Teams").doc("Project-Compost")
+    .set({
+      member1: "Loic",
+      member2: "Mith",
+      member3: "Qi",
+      member4: "V",
+      member5: "Harsh",
+    })
+    .then(() => {
+      console.log("Document successfully written!");
+    })
+    .catch((error) => {
+      console.error("Error writing document: ", error);
+    })
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <AuthUI />
         <p>
-          Check the <code>console</code> to monitor auth status.
+          Check the <code>console</code> to monitor auth and db status.
         </p>
       </header>
     </div >
