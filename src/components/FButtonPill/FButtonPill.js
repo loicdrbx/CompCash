75681decfd4b09d590bca03d1c67e98595a07af0
Harsh Patel <contact@harshpatel.ca>
2021-01-04 15:14:30 -0500

    home landing done

---
+++ src/components/FButtonPill/FButtonPill.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './FButtonPill.css';
+
+class FButtonPill extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    var tg = '';
+    if (this.props.newTab === true) {
+      tg = '_blank';
+    }
+
+    var buttonClass = 'button-pill';
+    if (this.props.style === 'dark') {
+      buttonClass = buttonClass + ' ' + 'button-pill-dark';
+    } else if (this.props.style === 'light') {
+      buttonClass = buttonClass + ' ' + 'button-pill-light';
+    } else if (this.props.style === 'darkOnLight') {
+      buttonClass = buttonClass + ' ' + 'button-pill-darkOnLight';
+    } else if (this.props.style === 'green') {
+      buttonClass = buttonClass + ' ' + 'button-pill-green';
+    }
+
+    return (
+      <a href={this.props.href} target={tg}>
+        <button
+          className={buttonClass}
+          id={this.props.id}
+          type={this.props.type}
+        >
+          {this.props.value}
+          <span className="button-pill-arrow">&gt;</span>
+        </button>
+      </a>
+    );
+  }
+}
+
+export default FButtonPill;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import AuthUI from '../../components/AuthUI';
+import './Home.css';
+import FButtonPill from '../../components/FButtonPill/FButtonPill';
+
+import img1 from '../../assets/Home/1.svg';
 
 function Home() {
   return (
     <div>
-      <section className="home-landing"></section>
+      <div className="home-landing container">
+        <div className="row align-items-center h-100">
+          <div className="col-12 col-md-7">
+            <h1>
+              Compost your waste and earn<span className="green">money</span>
+            </h1>
+            <p>
+              By composting your own waste, CompCash helps you earn quick and
+              easy cash while practicing a good deed for the environment.
+            </p>
+            <div className="row m-0">
+              <FButtonPill value="Learn More" href="#" style="dark" />
+              <div className="ml-3">
+                <FButtonPill value="Register Now" href="#" style="green" />
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-md-5">
+            <img src={img1} className="col" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
