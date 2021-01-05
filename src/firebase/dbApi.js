4a23dc854668ffa68e36a62f833460e3ca724fb4
Loic Darboux <loic.drbx@gmail.com>
2021-01-05 18:40:41 -0500

    dpApi template

--- src/firebase/dbApi.js
+++ src/firebase/dbApi.js
@@ -1,28 +1,43 @@
 import { db } from './index';
 
-/** Create a box */
-createBox = () => {
 
+/** Create a box */
+const createBox = ({ nickname, size, userId }) => {
+  // {
+  //   boxsize: 0,
+  //   nickname: "bla",
+  //   userId: "1234",  GET USERID from authApi
+  //   date: timestamp 
+  //   complete: true
+  // }
 }
 
 /** Get box data */
-getBoxData = () => {
-
+const getBoxData = (boxId) => {
+  // Get the entire document
+  // const nickname = doc.data().nickname
+  // const daysRemaining
+  // const percentComplete
+  // const estimatedIncome
+  // 
 }
 
-/** Get user data */
-getUserData = () => {
 
+/** Get user data */
+const getUserData = () => {
+  // const boxNumber;
+  // const boxIds
+  // const trees = impact * x
 }
 
 /** Increment user impact */
-incrementUserImpact = () => {
-
+const incrementUserImpact = (userId) => {
+  // increments a new box is added
 }
 
 export default {
   createBox,
   getBoxData,
+  incrementUserImpact,
   getUserData,
-  incrementUserImpact
 };
