811d3b731f98ef7f46be0ffe7a3d63d86a2acc83
Qi <wenq@myumanitoba.ca>
2021-01-06 15:06:28 -0600

    using react state to bind docs from firebase

--- src/components/DHome/DHome.js
+++ src/components/DHome/DHome.js
@@ -1,43 +1,71 @@
 import React from 'react';
 import './DHome.css';
+import * as dbAPI from '../../firebase/dbApi';
+import * as authAPI from '../../firebase/authApi';
+import { db } from '../../firebase/index';
 
 import DBoxDisplay from '../DBoxDisplay/DBoxDisplay';
 
-const boxesTestArray = [
-  {
-    boxTitle: "Amy's Box - Backyard Corner",
-    prcnt: '60',
-    daysLeft: '28',
-  },
-  {
-    boxTitle: "2Amy's Box - Backyard Corner",
-    prcnt: '20',
-    daysLeft: '18',
-  },
-];
+class DHome extends React.Component{
+  state = {
+      boxesTestArray: [],
+      boxesNum: 0
+  }
 
-const boxesNum = boxesTestArray.length;
+// const boxesTestArray = [
+//   {
+//     boxTitle: "Amy's Box - Backyard Corner",
+//     prcnt: '60',
+//     daysLeft: '28',
+//   },
+//   {
+//     boxTitle: "2Amy's Box - Backyard Corner",
+//     prcnt: '20',
+//     daysLeft: '18',
+//   },
+// ];
 
-function DHome() {
+//const boxesNum = boxesTestArray.length;
+
+  componentDidMount() {
+    let boxesTestArray = [];
+    this.state = { boxesNum: 2 };
+
+    db().collection("Boxes").get().then((snapshot) => (
+        snapshot.forEach((doc) => (
+            boxesTestArray.push({
+                id: doc.id,
+                boxTitle: doc.data().nickname,
+                prcnt: doc.data().boxsize
+            })
+        )),
+
+        this.setState({ boxesTestArray })
+      ));
+
+  }
+
+render() {
+
   return (
     <>
       <div className="dash-comp container pb-4">
         <h4 className="ml-4 pt-4">Boxes Summary</h4>
         <div className="pt-4">
-          {boxesTestArray.map((box) => (
-            <DBoxDisplay
-              boxTitle={box.boxTitle}
-              prcnt={box.prcnt}
-              daysLeft={box.daysLeft}
-            />
-          ))}
+        {this.state.boxesTestArray.map((box) => (
+          <DBoxDisplay
+            boxTitle={box.boxTitle}
+            prcnt={box.prcnt}
+            daysLeft={box.daysLeft}
+          />
+        ))}
         </div>
       </div>
       <div className="dash-comp container mt-4 pb-3">
         <div className="ml-4 pt-4">
           <h4>Estimated Income</h4>
           <p>
-            You have {boxesNum} boxes registered. With the sizes that you have
+            You have {this.state.boxesNum} boxes registered. With the sizes that you have
             selected, your estimated income will be
           </p>
           <h1>
@@ -61,5 +89,5 @@ function DHome() {
     </>
   );
 }
-
+}
 export default DHome;
--- src/firebase/dbApi.js
+++ src/firebase/dbApi.js
@@ -7,27 +7,33 @@ const createBox = ({ nickname, size, userId }) => {
   //   boxsize: 0,
   //   nickname: "bla",
   //   userId: "1234",  GET USERID from authApi
-  //   date: timestamp 
+  //   date: timestamp
   //   complete: true
   // }
 }
 
 /** Get box data */
-const getBoxData = (boxId) => {
+ const getBoxData = (boxId) => {
   // Get the entire document
+  // db().collection("Boxes").doc(boxId)
+  //   .onSnapshot((doc)=>{
+  //     console.log(doc.data());
+  //   });
   // const nickname = doc.data().nickname
   // const daysRemaining
   // const percentComplete
   // const estimatedIncome
-  // 
+  //
 }
 
 
 /** Get user data */
-const getUserData = () => {
+const getUserData = (usrId) => {
   // const boxNumber;
   // const boxIds
   // const trees = impact * x
+  //const doc = db().collection("users").doc(usrId).value;
+  // return db().collection("users").doc(usrId).get();
 }
 
 /** Increment user impact */
@@ -35,7 +41,7 @@ const incrementUserImpact = (userId) => {
   // increments a new box is added
 }
 
-export default {
+export {
   createBox,
   getBoxData,
   incrementUserImpact,
