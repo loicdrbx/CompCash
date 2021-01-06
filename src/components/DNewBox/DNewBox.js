791005c801f105f47bca92a300ccf500619e2d4b
Harsh Patel <contact@harshpatel.ca>
2021-01-05 21:13:36 -0500

    box form state done

--- src/components/DNewBox/DNewBox.js
+++ src/components/DNewBox/DNewBox.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './DNewBox.css';
 
 import FButtonPill from '../FButtonPill/FButtonPill';
@@ -9,60 +9,84 @@ import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 
-function DNewBox(props) {
-  return (
-    <div className="dash-comp container pb-3">
-      <Form className="ml-4">
-        <h4 className="pt-4">Add a new box</h4>
-        <Form.Group as={Row} controlId="formHorizontalEmail" className="mt-4">
-          <Form.Label column sm={2}>
-            Box Nickname
-          </Form.Label>
-          <Col sm={10}>
-            <Form.Control
-              type="text"
-              placeholder="Amy's Box - Backyard Corner"
-              className="form-text"
-            />
-          </Col>
-        </Form.Group>
-        <fieldset>
-          <Form.Group as={Row}>
-            <Form.Label as="legend" column sm={2}>
-              Box Size
+class DNewBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  state = {
+    nickname: '',
+    size: '',
+    error: '',
+    status: null,
+  };
+
+  handleChange = (e) => {
+    const value = e.target.value;
+    if (e.target.type == 'radio') {
+      this.setState({
+        [e.target.name]: e.target.id,
+      });
+    } else {
+      this.setState({
+        [e.target.name]: value,
+      });
+    }
+  };
+
+  handleSubmit = (e, props) => {
+    e.preventDefault();
+    console.log(this.state);
+    // Do something when the form is submitted
+  };
+
+  render() {
+    return (
+      <div className="dash-comp container pb-3">
+        <Form className="ml-4" onSubmit={this.handleSubmit}>
+          <h4 className="pt-4">Add a new box</h4>
+          <Form.Group as={Row} controlId="formHorizontalEmail" className="mt-4">
+            <Form.Label column sm={2}>
+              Box Nickname
             </Form.Label>
             <Col sm={10}>
-              <Form.Check
-                type="radio"
-                label="Small"
-                name="formHorizontalRadios"
-                id="formHorizontalRadios1"
-                className="radio-green"
-              />
-              <Form.Check
-                type="radio"
-                label="Medium"
-                name="formHorizontalRadios"
-                id="formHorizontalRadios2"
-              />
-              <Form.Check
-                type="radio"
-                label="Large"
-                name="formHorizontalRadios"
-                id="formHorizontalRadios3"
+              <Form.Control
+                type="text"
+                placeholder="Amy's Box - Backyard Corner"
+                className="form-text"
+                name="nickname"
+                onChange={this.handleChange}
               />
             </Col>
           </Form.Group>
-        </fieldset>
+          <fieldset onChange={this.handleChange}>
+            <Form.Group as={Row}>
+              <Form.Label as="legend" column sm={2}>
+                Box Size
+              </Form.Label>
+              <Col sm={10}>
+                <Form.Check type="radio" label="Small" name="size" id="small" />
+                <Form.Check
+                  type="radio"
+                  label="Medium"
+                  name="size"
+                  id="medium"
+                />
+                <Form.Check type="radio" label="Large" name="size" id="large" />
+              </Col>
+            </Form.Group>
+          </fieldset>
 
-        <Form.Group as={Row}>
-          <Col sm={{ span: 10, offset: 2 }}>
-            <FButtonPill style="green" value="Submit" />
-          </Col>
-        </Form.Group>
-      </Form>
-    </div>
-  );
+          <Form.Group as={Row}>
+            <Col sm={{ span: 10, offset: 2 }}>
+              <FButtonPill style="green" value="Submit" type="submit" />
+            </Col>
+          </Form.Group>
+        </Form>
+      </div>
+    );
+  }
 }
 
 export default DNewBox;
