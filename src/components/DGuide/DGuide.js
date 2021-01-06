77880fae134e99603fdbf98df9d0a71277219f3b
Harsh Patel <contact@harshpatel.ca>
2021-01-06 15:54:16 -0500

    Box Setup and box maintainence done

--- src/components/DGuide/DGuide.js
+++ src/components/DGuide/DGuide.js
@@ -106,7 +106,49 @@ function DGuide() {
       <div className="dash-comp container pb-4 mt-4">
         <div className="ml-4 pt-4">
           <h4>Box Setup</h4>
-          <h5>Step 1</h5>
+          <h5 className="mt-3">Step 1</h5>
+          <p>
+            Place twigs or alike materials on the bottom of your box. This layer
+            will allow for drainage and aeration. Now cover the first layer with
+            leaves.
+          </p>
+          <h5>Step 2</h5>
+          <p>
+            Alternate between layers of brown materials (carbon-rich) and green
+            materials (nitrogen-rich).
+          </p>
+        </div>
+      </div>
+      <div className="dash-comp container pb-4 mt-4">
+        <div className="ml-4 pt-4">
+          <h4>Box Maintenance</h4>
+          <h5 className="mt-3">Adding fresh material</h5>
+          <p>
+            When adding fresh material to your compost box, be sure to mix it in
+            with the lower layers. <br />
+            <span className="bold">
+              We also reccomend using our Compost Tool to know exactly what can
+              and cannot be compostable!
+            </span>
+          </p>
+
+          <h5>Controlling Moisture</h5>
+          <p>
+            Your compost box should be as wet as a{' '}
+            <span className="green bold">rung-out sponge</span>. <br />
+            <span className="">Add water to make the mixture more wet.</span>
+            <br />
+            <span className="">
+              Add dry materials like leaves or twigs to make the mixture more
+              dry.
+            </span>
+          </p>
+
+          <h5>Mix Regularly</h5>
+          <p>
+            Mix or turn the compost once a week to help the breakdown process
+            and eliminate odor.
+          </p>
         </div>
       </div>
     </>
