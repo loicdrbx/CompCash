5b1747ab6845c17173b36780ddb793e003c34859
Harsh Patel <contact@harshpatel.ca>
2021-01-05 20:39:41 -0500

    Done form FE

--- src/components/DNewBox/DNewBox.js
+++ src/components/DNewBox/DNewBox.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './DNewBox.css';
 
+import FButtonPill from '../FButtonPill/FButtonPill';
+
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
@@ -9,62 +11,53 @@ import Button from 'react-bootstrap/Button';
 
 function DNewBox(props) {
   return (
-    <div className="dash-comp container">
+    <div className="dash-comp container pb-3">
       <Form className="ml-4">
         <h4 className="pt-4">Add a new box</h4>
         <Form.Group as={Row} controlId="formHorizontalEmail" className="mt-4">
           <Form.Label column sm={2}>
-            Email
+            Box Nickname
           </Form.Label>
           <Col sm={10}>
-            <Form.Control type="email" placeholder="Email" />
-          </Col>
-        </Form.Group>
-
-        <Form.Group as={Row} controlId="formHorizontalPassword">
-          <Form.Label column sm={2}>
-            Password
-          </Form.Label>
-          <Col sm={10}>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              type="text"
+              placeholder="Amy's Box - Backyard Corner"
+              className="form-text"
+            />
           </Col>
         </Form.Group>
         <fieldset>
           <Form.Group as={Row}>
             <Form.Label as="legend" column sm={2}>
-              Radios
+              Box Size
             </Form.Label>
             <Col sm={10}>
               <Form.Check
                 type="radio"
-                label="first radio"
+                label="Small"
                 name="formHorizontalRadios"
                 id="formHorizontalRadios1"
+                className="radio-green"
               />
               <Form.Check
                 type="radio"
-                label="second radio"
+                label="Medium"
                 name="formHorizontalRadios"
                 id="formHorizontalRadios2"
               />
               <Form.Check
                 type="radio"
-                label="third radio"
+                label="Large"
                 name="formHorizontalRadios"
                 id="formHorizontalRadios3"
               />
             </Col>
           </Form.Group>
         </fieldset>
-        <Form.Group as={Row} controlId="formHorizontalCheck">
-          <Col sm={{ span: 10, offset: 2 }}>
-            <Form.Check label="Remember me" />
-          </Col>
-        </Form.Group>
 
         <Form.Group as={Row}>
           <Col sm={{ span: 10, offset: 2 }}>
-            <Button type="submit">Sign in</Button>
+            <FButtonPill style="green" value="Submit" />
           </Col>
         </Form.Group>
       </Form>
