30fc144961821d33895df1c1e796786dfae8d1ec
Harsh Patel <contact@harshpatel.ca>
2021-01-06 21:53:58 -0500

    Login Status Done

--- src/components/FNavbar/FNavbar.js
+++ src/components/FNavbar/FNavbar.js
@@ -9,10 +9,32 @@ import { IconContext } from 'react-icons';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
-import getUser from '../../firebase/authApi.js';
+import { auth } from '../../firebase/index';
 
 function FNavbar() {
-  console.log(getUser);
+  var user = auth().currentUser;
+  var renderNav;
+
+  // Check if there is a user logged in
+  if (user) {
+    console.log(user.displayName);
+    renderNav = (
+      <>
+        <LinkContainer to="/dashboard">
+          <Nav.Link className="navbar-link-text">Dashboard</Nav.Link>
+        </LinkContainer>
+        <p className="navbar-link-text my-2">{user.displayName}</p>
+        <img src={user.photoURL} className="profile-pic ml-2 mt-1" />
+      </>
+    );
+  } else {
+    renderNav = (
+      <LinkContainer to="/signin">
+        <Nav.Link className="navbar-link-text bold ">Login / Sign Up</Nav.Link>
+      </LinkContainer>
+    );
+  }
+
   return (
     <Navbar
       collapseOnSelect
@@ -31,12 +53,7 @@ function FNavbar() {
       />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className=""></Nav>
-        <Nav className="ml-auto">
-          <LinkContainer to="/dashboard">
-            <Nav.Link className="navbar-link-text">Dashboard</Nav.Link>
-          </LinkContainer>
-          <p className="navbar-link-text my-2">Signed in </p>
-        </Nav>
+        <Nav className="ml-auto">{renderNav}</Nav>
       </Navbar.Collapse>
     </Navbar>
   );
