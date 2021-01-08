62d7d8c34a8db641c379338655af9f264478d031
Harsh Patel <contact@harshpatel.ca>
2021-01-07 23:00:16 -0500

    Vision UI Done

--- src/components/DTool/DTool.js
+++ src/components/DTool/DTool.js
@@ -1,30 +1,31 @@
 import React, { useState } from 'react';
 import './DTool.css';
-import Clarifai from "clarifai";
+import Clarifai from 'clarifai';
 
 const app = new Clarifai.App({
-  apiKey: "41052665517141b6a60931faddba113b"
+  apiKey: '41052665517141b6a60931faddba113b',
 });
 
-const COMPOSTABLE_ITEMS = ["food", "fruit", "water"];
-const NON_COMPOSTABLE_ITEMS = ["plastic", "glass", "cutlery", "animal"];
+const COMPOSTABLE_ITEMS = ['food', 'fruit', 'water'];
+const NON_COMPOSTABLE_ITEMS = ['plastic', 'glass', 'cutlery', 'animal'];
 
 function DTool() {
-
+  const status = 0;
   const [compostable, setCompostable] = useState([]);
   const [nonCompostable, setNonCompostable] = useState([]);
-  const [source, setSource] = useState("");
+  const [source, setSource] = useState('');
 
   const handleCapture = (target) => {
     if (target.files) {
       if (target.files.length !== 0) {
+        status = 1;
         const file = target.files[0];
         // convert img file to a base64 string and make vision call
         const reader = new FileReader();
         reader.onloadend = () => {
           let b64Img = reader.result.replace(/^data:image.+;base64,/, '');
           analyseImg(b64Img);
-        }
+        };
         reader.readAsDataURL(file);
         // display uploaded picture
         const newUrl = URL.createObjectURL(file);
@@ -35,7 +36,7 @@ function DTool() {
 
   const analyseImg = (imgFile) => {
     app.models
-      .predict("aaa03c23b3724a16a56b629203edc62c", imgFile)
+      .predict('aaa03c23b3724a16a56b629203edc62c', imgFile)
       .then((response) => {
         let concepts = response.outputs[0].data.concepts;
         getCompostable(concepts);
@@ -45,23 +46,74 @@ function DTool() {
   };
 
   const getCompostable = (concepts) => {
-    let items = []
-    concepts.forEach(concept => {
-      if (COMPOSTABLE_ITEMS.includes(concept.name) && concept.value > 0.90) {
+    let items = [];
+    concepts.forEach((concept) => {
+      if (COMPOSTABLE_ITEMS.includes(concept.name) && concept.value > 0.9) {
         items.push(concept.name);
       }
-    })
+    });
     return setCompostable(items);
   };
 
   const getNotCompostable = (concepts) => {
-    let items = []
-    concepts.forEach(concept => {
-      if (NON_COMPOSTABLE_ITEMS.includes(concept.name) && concept.value > 0.90) {
+    let items = [];
+    concepts.forEach((concept) => {
+      if (NON_COMPOSTABLE_ITEMS.includes(concept.name) && concept.value > 0.9) {
         items.push(concept.name);
       }
-    })
+    });
     return setNonCompostable(items);
+  };
+
+  const compstableString = compostable.toString();
+  const nonCompstableString = nonCompostable.toString();
+
+  const DisplayItems = () => {
+    if (status === 1 && compstableString === '' && nonCompstableString === '') {
+      return (
+        <p className="bold">
+          Welp, looks like our "intelligent" model can't figure out the contents
+          of your image.
+        </p>
+      );
+    } else if (compstableString !== '' && nonCompstableString !== '') {
+      return (
+        <>
+          <p className="">
+            <span className="bold">
+              The following compostable items were identified:
+            </span>{' '}
+            {compstableString}
+          </p>
+          <p className="">
+            <span className="bold">
+              The following non-compostable items were identified:
+            </span>{' '}
+            {nonCompstableString}
+          </p>
+        </>
+      );
+    } else if (compstableString !== '') {
+      return (
+        <p className="">
+          <span className="bold">
+            The following compostable items were identified:
+          </span>{' '}
+          {compstableString}
+        </p>
+      );
+    } else if (nonCompstableString !== '') {
+      return (
+        <p className="">
+          <span className="bold">
+            The following non-compostable items were identified:
+          </span>{' '}
+          {nonCompstableString}
+        </p>
+      );
+    } else {
+      return null;
+    }
   };
 
   return (
@@ -72,10 +124,11 @@ function DTool() {
           Use the tool below to find out exactly what can and cannot be
           compostable!
         </p>
-        {source &&
+        {source && (
           <div display="flex" justifyContent="center" border={1}>
-            <img src={source} alt={"snap"}></img>
-          </div>}
+            <img src={source} alt={'snap'} className="prev-img"></img>
+          </div>
+        )}
         <input
           accept="image/*"
           id="icon-button-file"
@@ -83,8 +136,7 @@ function DTool() {
           capture="environment"
           onChange={(e) => handleCapture(e.target)}
         />
-        <p>The following compostable items were identified: {compostable.toString()}</p>
-        <p>The following non-compostable items were identified: {nonCompostable.toString()}</p>
+        <DisplayItems />
       </div>
     </div>
   );
